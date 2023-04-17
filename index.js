7368d10788baee40d47f4492cf58154fcd68ce56
AmjustGettingStarted <gamerhmv2050@gmail.com>
2023-04-18 05:12:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#mytext", {
+  strings: [
+    "JavaScript Animations",
+    "Animations in JavaScript",
+    "Using typed.js",
+    "Using AOS -> Animate On Scroll",
+  ],
+  typeSpeed: 100,
+});
+
+AOS.init({
+  duration: 1500,
+});
